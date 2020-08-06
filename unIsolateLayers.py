d9e58abf3ebf55e20836dd30b4fc16d9638b8504
M-VVV <68401718+M-VVV@users.noreply.github.com>
2020-08-06 22:08:37 +0200

    Update unIsolateLayers.py

--- unIsolateLayers.py
+++ unIsolateLayers.py
@@ -21,11 +21,9 @@ def unIsolateLayers():
     with open(temp_file_name, "r") as f:
         layers_data = json.load(f)
 
-    new_layers_data = {}
+    layers_data = {rs.LayerName(k):v for k, v in layers_data.items()}
 
-    for i in layers_data:
-        new_layers_data[rs.LayerName(i)] = layers_data[i]
-    sorted_layers_data = sorted(new_layers_data.iteritems())
+    sorted_layers_data = sorted(layers_data.iteritems())
 
     for layer_name, layer_visibility in sorted_layers_data:
         rs.LayerVisible(layer_name, layer_visibility)
